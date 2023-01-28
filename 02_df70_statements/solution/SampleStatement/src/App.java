3052af982b21045991631ccac1a98a3a997b5190
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2023-01-28 22:27:48 +0700

    2023-01-28

---
+++ 02_df70_statements/solution/SampleStatement/src/App.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+
+/*
+ * Author: Uaychai Chotjaratwanich
+ * Modified Date: 2023-01-28
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i; //Declare Vairable
+        i=5; //Assignment
+        System.out.println(i); //Call function or method
+    }
+}
---
+++ 03_f909_variables/solution/SampleVariable/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte a1 = 100;System.out.println(a1);
+        short a2 = 100;System.out.println(a2);
+        int a3 = 100;System.out.println(a3);
+        long a4 = 100l;System.out.println(a4);
+        float a5 = 100.00f;System.out.println(a5);
+        double a6 = 100.00;System.out.println(a6);
+        char a7 = 'a';System.out.println(a7);
+        String a8 = "Uaychai";System.out.println(a8);
+    }
+}

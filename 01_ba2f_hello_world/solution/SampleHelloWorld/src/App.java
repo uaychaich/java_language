8ef58ed48fb29ea6e6921c2cc0794b73017d4743
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2023-01-28 17:59:03 +0700

    2023-01-28

--- 01_ba2f_hello_world/solution/SampleHelloWorld/src/App.java
+++ 01_ba2f_hello_world/solution/SampleHelloWorld/src/App.java
@@ -1,3 +1,7 @@
+/*
+ * Author: Uaychai Chotjaratwanich
+ * Modified Date: 2023-01-28
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
